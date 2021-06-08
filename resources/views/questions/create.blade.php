7decbb00980b4f8f719248dea2517c8b2ac236b8
iambasanta <sangpangbasanta@gmail.com>
2021-06-08 13:18:45 +0545

    add ask question form

---
+++ resources/views/questions/create.blade.php
@@ -0,0 +1,48 @@
+@extends('layouts.app')
+
+@section('content')
+<div class="container">
+    <div class="row justify-content-center">
+        <div class="col-md-12">
+            <div class="card">
+                <div class="card-header">
+                    <div class="d-flex align-items-center">
+                        <h2>Ask Question</h2>
+                        <div class="ml-auto">
+                            <a href="{{route('questions.index')}}" class="btn btn-outline-secondary">Back to all questions</a>
+                        </div>
+                    </div>
+                </div>
+
+                <div class="card-body">
+                    <form action="{{route('questions.store')}}" method="post">
+                        @csrf
+                        <div class="form-group">
+                            <label for="question-title">
+                                Question Title
+                            </label>
+                            <input type="text" class="form-control @error('title') is-invalid @enderror" id="question-title" name="title">
+
+                            @error('title')
+                            <div class="alert alert-danger">{{ $message }}</div>
+                            @enderror
+                        </div>
+                        <div class="form-group">
+                            <label for="question-body">
+                                Explain your question
+                            </label>
+                            <textarea name="body" id="question-body" rows="10" class="form-control @error('body') is-invalid @enderror"></textarea>
+                            @error('body')
+                            <div class="alert alert-danger">{{ $message }}</div>
+                            @enderror
+                        </div>
+                        <div class="form-group">
+                            <button type="submit" class="btn btn-outline-primary btn-lg">Ask this question</button>
+                        </div>
+                    </form>
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
+@endsection
