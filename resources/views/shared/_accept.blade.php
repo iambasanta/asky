b7d28355b24458d52fb1e4729853be0de2002491
iambasanta <sangpangbasanta@gmail.com>
2021-06-11 21:01:55 +0545

    re-format accept file

--- resources/views/shared/_accept.blade.php
+++ resources/views/shared/_accept.blade.php
@@ -1,14 +1,14 @@
 @can('accept',$model)
-                        <a title="Mark this answer as best answer" class=" mt-2 {{ $model->status }}" onclick="event.preventDefault(); document.getElementById('accepted-answer-{{ $model->id }}').submit();">
-                            <i class="fas fa-check fa-2x"></i>
-                        </a>
-                        <form id="accepted-answer-{{ $model->id }}" action="{{route('answers.accept',$model->id)}}" method="POST" class="display:none;">
-                            @csrf
-                        </form>
-                        @else
-                        @if($model->is_best)
-                        <a title="Author of this question accepted this answer as best answer" class=" mt-2 {{ $model->status }}">
-                            <i class="fas fa-check fa-2x"></i>
-                        </a>
-                        @endif
-                        @endcan
+<a title="Mark this answer as best answer" class=" mt-2 {{ $model->status }}" onclick="event.preventDefault(); document.getElementById('accepted-answer-{{ $model->id }}').submit();">
+    <i class="fas fa-check fa-2x"></i>
+</a>
+<form id="accepted-answer-{{ $model->id }}" action="{{route('answers.accept',$model->id)}}" method="POST" class="display:none;">
+    @csrf
+</form>
+@else
+@if($model->is_best)
+<a title="Author of this question accepted this answer as best answer" class=" mt-2 {{ $model->status }}">
+    <i class="fas fa-check fa-2x"></i>
+</a>
+@endif
+@endcan
